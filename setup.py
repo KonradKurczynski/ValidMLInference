a0166c8f2aff3fe1802a55b00ed90d570a4f5316
kurczynsk1 <kkurczynski4@gmail.com>
2025-02-28 13:26:10 -0500

    -

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name="ValidMLInference",
-    version="0.0.1",
-    package_dir={'': 'src'},
-    packages=find_packages(where='src'),
-    install_requires=["numpy", "scipy", "numdifftools", "jax", "jaxopt"],
-)
