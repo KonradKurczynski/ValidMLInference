4d443ee178480d0cc9edb9abd85778eaec3fa553
kurczynsk1 <kkurczynski4@gmail.com>
2025-05-28 20:56:35 -0400

    updated interfaces

--- src/ValidMLInference/__init__.py
+++ src/ValidMLInference/__init__.py
@@ -1,3 +1,3 @@
-from .aux import ols_bca, one_step, ols_bcm, ols_bca_topic, ols_bcm_topic, ols
+from .interfaces import ols_bca, one_step, ols_bcm, load_dataset, one_step_gaussian_mixture, ols
 
-__all__ = ["ols_bca", "ols_bcm", "ols_bca_topic", "ols_bcm_topic", "one_step", "ols"]
+__all__ = ["ols_bca", "ols_bcm", "ols_bca_topic", "ols_bcm_topic", "one_step", "load_dataset", "one_step_gaussian_mixture", "ols"]
